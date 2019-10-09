a83931e772a40f1a0c00e0b82b707ecd368aed25
James Batsell <jamesbatsell05@gmail.com>
2019-10-09 06:20:59 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,14 +1,16 @@
 class Backer
-  attr_accessor :backed_projects
+  attr_accessor :backed_project
   attr_reader :name
   
   
   
   def initialize(name)
     @name = name
-    @backed_projects = []
+    @backed_project = []
   end
   
-  def backed_project(Projects)
+  def backed_project(Project)
+    @backed_project << Project
+  end
   
 end
